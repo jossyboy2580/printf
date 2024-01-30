a85298cfe1da2444db23e487dee76dbe2986261b
Ajobiewe Joseph <joseph4hacking@gmail.com>
2024-01-30 20:04:57 +0100

    Trying out Chim solution again to compare the errors

--- main.h
+++ main.h
@@ -1,19 +1,15 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
 #include <stdarg.h>
-#include <stdlib.h>
 #include <unistd.h>
-
+#include <string.h>
 int _printf(const char *format, ...);
 
-void handle_decimal(char **buffer, char *spec, va_list val);
-void handle_string(char **buffer, char *spec, va_list val);
-void handle_percent(char **buffer, char *spec, va_list val);
-void handle_char(char **buffer, char *spec, va_list val);
 
-void append_string(char **str, char *str2);
-void append_char(char **str, char c);
+
+
+
+
 
 #endif
