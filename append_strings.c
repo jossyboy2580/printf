f956348a0622590694831ab2bfd479a050694318
Ajobiewe Joseph <joseph4hacking@gmail.com>
2024-01-31 08:34:30 +0100

    adjusted some functions to solve a bug

--- append_strings.c
+++ append_strings.c
@@ -68,3 +68,28 @@ void append_char(char **str1, char c)
 	(*str1)[len_str1] = c;
 	(*str1)[len_str1 + 1] = '\0';
 }
+
+
+/**
+ * rev_string - Reverse a string with this function
+ * @s: The string we want to reverse
+ */ 
+
+void rev_string(char *s)
+{
+	int len = 0, i, half;
+	char temp;
+
+	while (s[len] != '\0')
+		len++;
+
+	half = len / 2;
+	if (len % 2 != 0)
+		half = (len - 1) / 2;
+	for (i = 0; i < half; i++)
+	{
+		temp = *(s + i);
+		*(s + i) = *(s + (len - (i + 1)));
+		*(s + (len - (i + 1))) = temp;
+	}
+}
--- handlers.c
+++ handlers.c
@@ -46,7 +46,6 @@ void handle_char(char **buffer, char *spec, va_list val)
 
 	(void)(spec);
 	c = va_arg(val, int);
-	printf("char is %c\n", c);
 	append_char(buffer, c);
 }
 
@@ -64,5 +63,35 @@ void handle_decimal(char **buffer, char *spec, va_list val)
 
 	append_string(buffer, str_arg);
 	free(str_arg);
+	free(spec);
+}
+
+/**
+ * handle_binary - Handle all binary conversions
+ *
+ * @buffer: The destination for storing the chars
+ * @spec: The specifications to use
+ * @val: Variadic functions list macro
+ */
+void handle_binary(char **buffer, char *spec, va_list val)
+{
+	unsigned int arg = va_arg(val, unsigned int);
+	char *bin = NULL;
+	int len = 1;
+	unsigned int rem;
+
+	while (arg)
+	{
+		bin = realloc(bin, sizeof(char) * ++len);
+		if (!bin)
+			return;
+		rem = arg % 2;
+		arg = arg / 2;
+		bin[len - 2] = rem - '0';
+	}
+	bin[len - 1] = '\0';
+	rev_string(bin);
+	append_string(buffer, bin);
+	free(bin);
 	free(spec);
 }
