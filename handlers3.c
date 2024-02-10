9ddc69f4a78126969ac43a49522098b70515b5a7
Chimaobi Ubani <wiztherealist@gmail.com>
2024-02-10 10:05:06 -0800

    null

--- handlers3.c
+++ handlers3.c
@@ -7,6 +7,7 @@
  * @spec: The specifications to use
  * @val: Variadic functions list macro
  */
+
 void handle_uint(char **buffer, char *spec, va_list val)
 {
 	unsigned long int arg = va_arg(val, unsigned long int);
